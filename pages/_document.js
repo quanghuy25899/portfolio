f311b0725bc77048189161f72c36cc703804359d
Quang Pham <quanghuy25899@gmail.com>
2020-07-28 18:10:36 -0700

    minor change

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,7 @@ export default class MyDocument extends Document {
         return (
             <html>
                 <Head>
+                    <title>Brian Pham's e-portfolio</title>
                     <meta
                         name="viewport"
                         content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1, user-scalable=no, shrink-to-fit=no"
