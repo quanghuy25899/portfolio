5823c618173e90f5910b22f074e1fe58e44a1277
quang <quanghuy25899@gmail.com>
2020-10-18 23:33:13 -0700

    updated UI

--- modules/staticPages/components/heading/HeadingContent.js
+++ modules/staticPages/components/heading/HeadingContent.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Row, Col } from 'antd';
+import styled from 'styled-components';
+
+const StyledRow = styled(Row)`
+    background-color: #008060;
+    color: white;
+    margin-top: 60px;
+`;
 
 const HeadingContent = () => {
     return (
-        <Row style={{ backgroundColor: '#008060', color: 'white' }}>
+        <StyledRow>
             <Col xs={24} md={8} lg={7}>
                 <img src="static/img/profilePic.jpg" alt="Profile" style={{ width: 200, height: 260 }} />
             </Col>
@@ -15,7 +22,7 @@ const HeadingContent = () => {
                     <p>(+1) 778-922-6999 * brianpham2589@gmail.com</p>
                 </div>
             </Col>
-        </Row>
+        </StyledRow>
     );
 };
 
